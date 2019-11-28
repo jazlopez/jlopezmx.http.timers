52afa8ce35dcc03d25775d22d951ae4057eb386e
Jaziel Lopez <juan.jaziel@gmail.com>
2019-11-28 00:01:32 -0800

    npm run start

---
+++ main.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright [2019] [Jaziel Lopez, Software Engineer, BC MX ]
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var liveServer = require("live-server");
+
+var params = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    // root: ".", // Set root directory that's being served. Defaults to cwd.
+    open: false, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    // file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    // mount: [['/node_modules', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
